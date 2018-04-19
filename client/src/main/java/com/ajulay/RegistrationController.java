4802334ee7809497d74e4cfb18d4070cfedbcdaa
Ajulay <11v09v@gmail.com>
2018-04-19 08:57:46 +0300

    add login window

--- client/src/main/java/com/ajulay/RegistrationController.java
+++ client/src/main/java/com/ajulay/RegistrationController.java
@@ -1,7 +1,50 @@
 package com.ajulay;
 
-/**
- * Created by ajulay on 19.04.2018.
- */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class RegistrationController {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    @FXML
+    TextField login, password, nick;
+    @FXML
+    Label result;
+    public void tryToRegister() {
+
+        try {
+            if (socket == null || socket.isClosed()) {
+                socket = new Socket("localhost", 8189);
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+
+                out.writeUTF("/registration " + login.getText() + " " + password.getText() + " " + nick.getText());
+                String answer = in.readUTF();
+                result.setText(answer);
+
+            }
+        } catch (IOException e) {
+            System.out.println("reading error...");
+            // e.printStackTrace();
+        } finally {
+
+            try {
+                socket.close();
+                in.close();
+                out.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
